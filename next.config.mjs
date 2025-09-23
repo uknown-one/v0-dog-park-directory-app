352c20935d9f0ee6db3891b559e50aa6c172582c
uknown-one <jpmares1031@gmail.com>
2025-09-22 21:31:50 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Static export is incompatible with API routes and NextAuth
-  // output: 'export',
+  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
   // Uncomment and modify these lines if deploying to a repository subdirectory
-  // basePath: '/dog-park-directory',
-  // assetPrefix: '/dog-park-directory/',
+  basePath: '/dog-park-directory',
+  assetPrefix: '/dog-park-directory/',
   eslint: {
     ignoreDuringBuilds: true,
   },
